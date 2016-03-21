8a9b1d43c64cf8eee2444cf50842e5141fa71390
Prelang Builder <builder@prelang.com>
2016-03-21 08:44:09 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,5 +1,5 @@
 class GraphsController < ApplicationController
-  before_action :set_graph, only: [:show, :edit, :update, :destroy]
+  before_action :set_graph, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /graphs
   # GET /graphs.json
