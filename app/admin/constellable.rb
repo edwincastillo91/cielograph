d8103837aec62b26b90e0ca308df3d8a6cae993d
Prelang Builder <builder@prelang.com>
2016-03-21 08:44:02 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Constellable' to allow for create, update

--- app/admin/constellable.rb
+++ app/admin/constellable.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Constellable do
+  permit_params :constellable_type, :constellable_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
